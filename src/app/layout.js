162453603f130288e40ea4929a0e6bdd6f303959
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-07-18 12:05:31 +0500

    Added Fonts from Google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Mulish } from 'next/font/google'
+ 
+const mulish = Mulish({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={mulish.className}>
         <Header />
         {children}
       </body>
