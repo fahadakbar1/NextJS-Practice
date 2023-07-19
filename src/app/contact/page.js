2dec3a85d35f5b0b4fb99e49e5c89b508de409e7
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-07-19 22:43:43 +0500

    Added a contact page Made an api for contact form Made scheme for contact form submission Added map

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,15 +1,35 @@
-import ContactCard from "../components/ContactCard";
-import ContactForm from "../components/ContactForm";
-import styles from './contact.module.css'
+import React from "react";
+import ContactCard from "@/app/components/ContactCard";
+import styles from "./contact.module.css";
+import ContactForm from "@/app/components/ContactForm";
 
 const Contact = () => {
-    return (
-      <>
+  return (
+    <>
+      <div className={styles.container}>
         <h1>Contact Us</h1>
-        <ContactCard/>
-        <ContactForm/>
-      </>
-    );
-  };
-  
-  export default Contact;
+        <ContactCard />
+
+        <section className={styles.contact_section}>
+          <h2>
+            We'd love to hear <span> from you </span>
+          </h2>
+
+          <ContactForm />
+        </section>
+      </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1917.1154934417568!2d73.01936589873868!3d33.576833455297795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df93853e0687ab%3A0x89977131a2d21935!2sG.care%20chemist!5e0!3m2!1sen!2s!4v1689788277976!5m2!1sen!2s"
+        width="600"
+        height="450"
+        style={{ border: 0, width: "100%" }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+};
+
+export default Contact;
