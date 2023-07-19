9ddd5ffc1d9ad56c5e3125530367e328af97aba2
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-07-19 10:46:56 +0500

    Added contact cards to contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,13 @@
-const Page = () => {
+import ContactCard from "../components/ContactCard";
+import styles from './contact.module.css'
+
+const Contact = () => {
     return (
       <>
         <h1>Contact Us</h1>
+        <ContactCard/>
       </>
     );
   };
   
-  export default Page;
+  export default Contact;
